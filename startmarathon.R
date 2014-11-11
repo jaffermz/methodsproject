2af82deca560e336d69149fdcd76fb121ca66561
jaffer_mz@hotmail.com <jaffer_mz@hotmail.com>
2014-11-11 12:44:32 -0500

    beforelunch

--- startmarathon.R
+++ startmarathon.R
@@ -1,3 +1,16 @@
 dim(marathon)
 names(marathon)
 class(marathon)
+
+##checking NA values
+missingrows <- c()
+for(i in 1:dim(marathon)[1])
+{
+  check <- marathon[i,]
+  missingyn <- is.na(check)
+  if(any(missingyn))
+  {
+    missingrows <- c(missingrows, i)
+  }
+}
+##
